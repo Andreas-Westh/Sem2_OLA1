c99eebf9a5d3107a1a1dff036d504c51bb61bf94
60gCreatine <cphah643@gmail.com>
2025-02-18 09:36:54 +0100

    added tasks

---
+++ Opgave_1.R
@@ -0,0 +1,18 @@
+#### Opgave 1.1 – MongoDB ####
+#Overvej hvordan man kan eksekvere mongo-queries i et script således at man kan få alle records med ud. 
+#Eksekvér den med alle skud fra angribere i et script.
+
+
+##### Opstil en query i R for at hente følgende fra fodboldkampe liggende i MongoDB: #####
+
+
+###### 1. Der finder ”RKC Waalwijk”’s hjemmekampe. ######
+
+
+###### 2. Der finder antal skud for den hollandske og polske liga ###### 
+
+
+###### 3. Der finder alle skud fra angribere i alle kampe og printer player samt shot-egenskaberne isGoal og onTarget.######
+
+
+###### 4. Der finder antal spillere som er over 190 høj. ###### 
