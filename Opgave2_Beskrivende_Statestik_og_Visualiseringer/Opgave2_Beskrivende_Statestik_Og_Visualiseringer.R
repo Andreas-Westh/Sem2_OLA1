50a15594525df94da048abefa4f36ff7bb4b4a65
60gCreatine <cphah643@gmail.com>
2025-02-21 01:29:44 +0100

    team ranking webscrape

--- Opgave2_Beskrivende_Statestik_og_Visualiseringer/Opgave2_Beskrivende_Statestik_Og_Visualiseringer.R
+++ Opgave2_Beskrivende_Statestik_og_Visualiseringer/Opgave2_Beskrivende_Statestik_Og_Visualiseringer.R
@@ -1,10 +1,31 @@
+library(rvest)
+
 #### Opgave 2.1 - Afleveringer ####
 # Lav beskrivende statistik på afleveringer i henholdsvis den polske og hollandske liga. 
 # I repræsenterer IKKE et unikt hold, men er alene ekstern analytiker ansat hos fx Wyscout. 
 # Der er minimum fem elementer som det ønskes I kigger nærmere på:
 
 ##### 1. Hvordan er afleveringerne for henholdsvis top og bund 5 holdene for sæsonen 2021/2022 i den polske og hollandske liga? #####
+# https://www.eurosport.com/football/eredivisie/2023-2024/standings.shtml
 
+###### Simple Team Ranking WebScrape ######
+      # URL for rankings
+      url <- "https://www.eurosport.com/football/eredivisie/2023-2024/standings.shtml"
+      
+      # read the page html
+      page <- read_html(url)
+      
+      # extract the teams name
+      teams_ranking <- page %>% 
+        html_nodes('a.absolute.right-1.max-w-full.truncate.left-8.lg\\:caps-s5-fx.hidden.md\\:block') %>% 
+        html_text(trim = TRUE) %>% as.data.frame()
+      
+      # since the df is already in order, row name == ranking
+      teams_ranking$ranking <- row.names(teams_ranking)
+      colnames(teams_ranking) <- c("team","ranking")
+
+top_5 <- head(teams_ranking$team,5)
+bottom_5 <- tail(teams_ranking$team,5)
 
 
 
