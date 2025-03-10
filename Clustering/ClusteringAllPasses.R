9090a1c5e70b1f5ac595539aee882322333cc23e
60gCreatine <cphah643@gmail.com>
2025-03-10 23:54:47 +0100

    start for clustering

--- Clustering/ClusteringAllPasses.R
+++ Clustering/ClusteringAllPasses.R
@@ -5,6 +5,7 @@ library(ggplot2)
 library(plotly)
 library(ggsoccer)
 library(rpart)
+library(factoextra)
 
 allmatches <- readRDS("Shiny/allmatches.rds")
 allshots <- readRDS("Shiny/allshots.rds")
@@ -20,19 +21,59 @@ clustering_vars <- allpasses[, c("pass.angle", "pass.length")]
 
 allpasses_scaled <- as.data.frame(scale(clustering_vars))
 
-
+# correlations
 passes_Corr <- cor(allpasses_scaled)
 corrplot::corrplot(passes_Corr,addCoef.col = "black",method = "square",type = "lower")
 
+allpasses_test <- allpasses %>% 
+  group_by(player.name, matchId) %>%
+  summarise(player_passes = n(), .groups = "drop") %>%  # Antal afleveringer pr. kamp
+  group_by(player.name) %>%
+  summarise(player_avgpass = mean(player_passes, na.rm = TRUE))  # Gennemsnit pr. spiller
+
+allpasses <- allpasses %>%
+  left_join(allpasses_test, by = "player.name")
 
 
+df <- allpasses[, c("pass.angle","pass.length","player_avgpass")]
+df_scaled <- as.data.frame(scale(df))
 
-# correlations
+set.seed(1970)
+sample_size <- 50000
+df_sampled <- df_scaled[sample(1:nrow(df_scaled), sample_size), ]
 
-
-# kmeans
-
-
+# Kmeans
+for (i in 1:20) {
+  tmod <- kmeans(df_sampled, centers = i, nstart = 10, iter.max = 500)
+  dftwss[i, 'twss'] <- tmod$tot.withinss
+}
 # elbow
+plot(dftwss)
+
+ggplot(dftwss, aes(x = k, y = twss)) +
+  geom_line(color = "blue") +
+  geom_point(color = "red") +
+  labs(title = "Elbow Method for Optimal K",
+       x = "Number of Clusters (K)",
+       y = "Total Within-Cluster Sum of Squares") +
+  theme_minimal()
 
 
+#kmeans cluster
+kmod <- kmeans(df_scaled, nstart = 10, centers = 4)
+fviz_cluster(kmod, data = df_scaled)
+df$cluster <- as.factor(kmod$cluster)
+
+clusters <- df %>% group_by(cluster) %>%
+  summarise(
+    pass.angle = mean(pass.angle),
+    pass.length = mean(pass.length),
+    player_avgpass = mean(player_avgpass),
+    count = n()
+  )
+
+# pca
+data.pca <- princomp(df_sampled)
+summary(data.pca)
+data.pca$loadings[, 1:3]
+fviz_pca_var(data.pca, col.var = "black")
